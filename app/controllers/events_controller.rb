0e4defed2046180536c13548950620f949fae2f4
DamirSvrtan <damir.svrtan@gmail.com>
2013-09-16 22:03:05 +0200

    removed the script from index controller action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
 
 	APP_ID = "507711889323733"
 	APP_SECRET = "c92a6204df0660605ec52ca4cae9a2a5"
-	APP_ACCESS_TOKEN = "CAAHNwuLPttUBAGHO0ezLLr21cgTZChtLFQcK738TvoiACxEObF614ZCZCKmpHPW8VrXWfdlviThJS541918IOwAxeS8ZAoZCBQZC4KGCtlDOl1JW3FvSvt5KFsxH9opOJWLZA0IndK74GXjL4RdI35K27CQMqMGBzUBDctUrprhWaJzVxxDA9FRvaiQtmNy6TwZD"
+	APP_ACCESS_TOKEN = "CAAHNwuLPttUBAKR2kNZCMfIISZCpd0ajwU0vvI3HOdkkYPcrBikrB3Oa2SnWluasLiegJ9FQZBtmfTzoeUmyu7RDWHGP7XuX2t0Rfw1ZBYDprsOIANDXvXOWxK2qFgdPYXr8q5S8yAhpERBCR49tqvZBfZAtDkmjR919FbL95KXURQZBWd1A96K"
 	
 	before_filter {	@graph = Koala::Facebook::API.new(APP_ACCESS_TOKEN) }
 
@@ -10,10 +10,9 @@ class EventsController < ApplicationController
 	#	@oauth = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, "http://localhost:3000/")
 	#	@access_token = @oauth.get_app_access_token
 		@events = @graph.get_connections("334751420002389", "events")
-		#event_ids = @events.map {|event| event["id"] }
-		@events.each do |event|
-			Event.create_event(event) if Event.where(fb_id: event["id"]).first.nil?
-		end
+		#@events.each do |event|
+		#	Event.create_event(event) if Event.where(fb_id: event["id"]).first.nil?
+		#end
 	end
 
 	def show
