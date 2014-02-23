8fc6fb71c59e21ca7bb65c2d442526294703572d
damir <damir.svrtan@gmail.com>
2014-02-23 22:59:40 +0100

    Better date selection

--- app/services/events.rb
+++ app/services/events.rb
@@ -3,7 +3,7 @@ class Events
   def self.all
       graph = AccessTokenHandler.get_graph_object
       events = graph.get_connections("334751420002389", "events", {fields: "start_time,name,location,id,description"})
-      events.select! {|event| event["start_time"] > Date.yesterday.to_s}
+      events.select! {|event| Date.parse(event["start_time"]) > Date.yesterday}
       events.reverse!
   end
 
