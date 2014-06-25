6f9740a8709999983c4b69be7b1494f265b2f928
Damir Svrtan <damir.svrtan@gmail.com>
2014-06-25 14:01:38 +0200

    Indentation fix.

--- app/services/events.rb
+++ app/services/events.rb
@@ -1,10 +1,10 @@
 class Events
 
   def self.all
-      graph = AccessTokenHandler.get_graph_object
-      events = graph.get_connections("334751420002389", "events", {fields: "start_time,name,location,id,description"})
-      events.select! {|event| Date.parse(event["start_time"]) > Date.yesterday}
-      events.reverse!
+    graph = AccessTokenHandler.get_graph_object
+    events = graph.get_connections("334751420002389", "events", {fields: "start_time,name,location,id,description"})
+    events.select! {|event| Date.parse(event["start_time"]) > Date.yesterday}
+    events.reverse!
   end
 
 end
